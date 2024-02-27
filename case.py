6376cbcf2168bafa050ecbcb23178edea7374d94
Darialr <darial.ryaguzova@gmail.com>
2024-02-27 21:03:12 +0700

    for sure last change

--- case.py
+++ case.py
@@ -10,16 +10,15 @@ def main():
     Main function.
     :return: None
     """
-
-    spd_perry = 10
-    spd_bike = 60
     grandma_dis = 0.005
     dock_dis = 10
     river_dis_1 = 10
     shark_time = 0.25
     turn_rnd_dis = 5
-    spd_strm = 5
     river_dis_2 = 10
+    spd_perry = 10
+    spd_bike = 60
+    spd_stream = 5
 
     start_time = input(ru.START_TIME)
     hours, minutes = map(int, start_time.split(":"))
@@ -27,10 +26,10 @@ def main():
     total_time_hour = 0
     total_time_hour += grandma_dis / spd_perry
     total_time_hour += dock_dis / spd_perry
-    total_time_hour += river_dis_1 / (spd_bike + spd_strm)
+    total_time_hour += river_dis_1 / (spd_bike + spd_stream)
     total_time_hour += shark_time
-    total_time_hour += turn_rnd_dis / (spd_bike - spd_strm)
-    total_time_hour += river_dis_2 / (spd_bike + spd_strm)
+    total_time_hour += turn_rnd_dis / (spd_bike - spd_stream)
+    total_time_hour += river_dis_2 / (spd_bike + spd_stream)
 
     total_time_min = int(total_time_hour * 60)
     hours_time = int(total_time_hour)
@@ -38,6 +37,7 @@ def main():
 
     finish_hrs = hours + hours_time
     finish_min = minutes + minutes_time
+
     if finish_min >= 60:
         finish_min -= 60
         finish_hrs += 1
